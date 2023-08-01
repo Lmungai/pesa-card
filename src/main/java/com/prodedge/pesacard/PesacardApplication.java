b5ceddaf568809750cb249c48b98dbd6cc2edf3c
Lee Mungai <Lee.mungai@gmail.com>
2023-08-01 07:45:03 +0300

    Initial Commit

---
+++ src/main/java/com/prodedge/pesacard/PesacardApplication.java
@@ -0,0 +1,13 @@
+package com.prodedge.pesacard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesacardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesacardApplication.class, args);
+	}
+
+}
